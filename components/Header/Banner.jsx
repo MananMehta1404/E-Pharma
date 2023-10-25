0a9c8fab5d65bdd1c594f7a8d8c1a76c6e394444
MananMehta1404 <mehtamanan1442003@gmail.com>
2023-10-25 23:23:21 +0530

    Creating signin and signup pages

--- components/Header/Banner.jsx
+++ components/Header/Banner.jsx
@@ -15,8 +15,8 @@ const Banner = () => {
 
                     <div className="flex flex-col space-y-2">
                         <Image src="/assets/banner/medal.png" alt="banner" width={100} height={100} />
-                        <h1 className="poppins text-gray-700 font-semibold text-3xl lg:text-3xl leading-relaxed">Best Quality <br /> <span className="text-5xl">Medicine in 2021</span></h1>
-                        <p className="text-gray-500 text-light text-sm">Our products are world best product. We sell the real products.<br/> Welcome to our shop.</p>
+                        <h1 className="poppins text-gray-700 font-semibold text-3xl lg:text-3xl leading-relaxed">Your Health, Our Priority – <br /> <span className="text-5xl">Delivering Wellness to Your Doorstep!</span></h1>
+                        <p className="text-gray-500 text-light text-sm">Experience the World's Finest Products – Authenticity Guaranteed!<br/>Discover Our Store.</p>
                     </div>
 
                     <Button className="btn-primary py-3 px-4 poppins w-48 mt-6" text="Explore our shop" />
